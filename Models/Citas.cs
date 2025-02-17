e5d1d23c90ecc59c4aaae076b17ea1bdf7994f13
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-16 20:22:42 -0400

    Creando la tabla de Citas

---
+++ Models/Citas.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp1.Models;
+
+public class Citas
+{
+    [Key]
+    public int CitaId { get; set; }
+
+    public DateTime Fecha { get; set; }
+    public int MascotaId { get; set; }
+    [ForeignKey("MascotaId")]
+    public virtual Mascotas Mascotas { get; set; }
+
+    public int EmpleadoId { get; set; }
+    [ForeignKey("EmpleadoId")]
+    public virtual Empleados Empleados { get; set; }
+
+}
