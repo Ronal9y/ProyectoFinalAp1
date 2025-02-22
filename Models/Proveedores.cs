15271fbb3226eb5b5b94f11d12c34e6e75a2f198
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-21 22:30:37 -0400

    Creando la tabla de proveedores con relacion a productos

---
+++ Models/Proveedores.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp1.Models;
+
+public class Proveedores
+{
+    [Key]
+    public int ProveedorId { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
+    public string? Nombre { get; set; }
+    [Required(ErrorMessage = "El campo fecha es obligatoria.")]
+    [DataType(DataType.Date)]
+    public DateTime FechaDeEntrega { get; set; }
+
+    [Required(ErrorMessage = "Favor colocar la dirección.")]
+    [RegularExpression(@"^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s,.#-]+$", ErrorMessage = "La dirección contiene caracteres no válidos.")]
+    public string? Direccion { get; set; }
+    [Required(ErrorMessage = "Favor colocar el teléfono.")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "El número de teléfono debe contener exactamente 10 dígitos.")]
+    public string? Telefono { get; set; }
+    [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+    [EmailAddress(ErrorMessage = "Debe ingresar un correo electrónico válido.")]
+    public string? Email { get; set; }
+
+    public virtual ICollection<Productos> Producto { get; set; } = new List<Productos>();
+}
