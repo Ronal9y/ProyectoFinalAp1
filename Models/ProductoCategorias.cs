ddb6f38b35ccbde7466b446562ee2db1484f9819
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-21 22:44:45 -0400

    Creacion de la tabla ProductoCategoria

---
+++ Models/ProductoCategorias.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp1.Models;
+
+public class ProductoCategorias
+{
+    [Key]
+    public int CategoriaId { get; set; }
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
+    public string? Nombre { get; set; }
+
+    public string? IconCSS { get; set; }
+
+    public virtual ICollection<Productos> Producto { get; set; } = new List<Productos>();
+}
