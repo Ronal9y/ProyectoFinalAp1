80db503d946d8ded5e6ed70039b30ff8dfed8b04
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-23 17:15:01 -0400

    Dtos Productos categorias

---
+++ Models/Dtos/ProductoCategoriasDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAp1.Models.Dtos
+{
+    public class ProductoCategoriasDto
+    {
+
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? IconCSS { get; set; }
+    }
+}
