5154d78247ba8d7b1a6750680a6b00fba4d8836e
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-23 17:28:46 -0400

    Dtos mascota para agregar

---
+++ Models/Dtos/MascotaParaAgregarDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAp1.Models.Dtos
+{
+    public class MascotaParaAgregarDto
+    {
+
+        public int CarritoId { get; set; }
+        public int MascotaId { get; set; }
+        public int Cantidad { get; set; }
+
+    }
+}
