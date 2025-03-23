e7c331e70c8d7c5163fd3b9d19fd484a1dad6548
DELL <DELL@DESKTOP-7VMN3MI>
2025-03-23 10:11:44 -0400

    se modifico el nombre y caneccion de esta clase

---
+++ Models/CarritoMascotas.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp1.Models
+{
+    public class CarritoMascotas
+    {
+        [Key]
+        public int CarritoMascotaId { get; set; }
+
+        [Required]
+        public int MascotaId { get; set; }
+        [ForeignKey("MascotaId")]
+        public virtual Mascotas Mascota { get; set; }
+
+        [Required(ErrorMessage = "La cantidad es obligatoria.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0.")]
+        public int Cantidad { get; set; }
+       
+    }
+}
