6f6a2e153c5d8432c5c02fcf7b6806fb839a6291
DELL <DELL@DESKTOP-7VMN3MI>
2025-03-23 10:11:03 -0400

    se creo el carrito

--- Models/Carrito.cs
+++ Models/Carrito.cs
@@ -7,11 +7,13 @@ public class Carrito
 {
     [Key]
     public int CarritoId { get; set; }
-    public int UsuarioId { get; set; }
-    [ForeignKey("UsuarioId")]
-    public virtual Usuarios Usuario { get; set; }
-    public DateTime Fecha { get; set; }
 
-    public virtual ICollection<Articulos> Articulo { get; set; } = new List<Articulos>();
-    public virtual ICollection<ArticuloMascotas> ArticuloMascota { get; set; } = new List<ArticuloMascotas>();
+    [Required]
+    public int ProductoId { get; set; }
+    [ForeignKey("ProductoId")]
+    public virtual Productos Producto { get; set; }
+
+    [Required(ErrorMessage = "La cantidad es obligatoria.")]
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0.")]
+    public int Cantidad { get; set; }
 }
