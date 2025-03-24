81d33b1ef1b89499c8d9db843028073dd8b9e1a6
DELL <DELL@DESKTOP-7VMN3MI>
2025-03-24 11:04:00 -0400

    se corrigieron las restricciones

--- Models/Carrito.cs
+++ Models/Carrito.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ProyectoFinalAp1.Models;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProyectoFinalAp1.Models;
@@ -6,14 +7,15 @@ namespace ProyectoFinalAp1.Models;
 public class Carrito
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int CarritoId { get; set; }
-
-    [Required]
-    public int ProductoId { get; set; }
-    [ForeignKey("ProductoId")]
-    public virtual Productos Producto { get; set; }
 
     [Required(ErrorMessage = "La cantidad es obligatoria.")]
     [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0.")]
     public int Cantidad { get; set; }
+    public int ProductoId { get; set; }
+    [ForeignKey("ProductoId")]
+    public virtual Productos Producto { get; set; }
+
+    
 }
