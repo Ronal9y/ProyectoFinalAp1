e2072ec228a6c9177520edefd9b208a5497945df
DELL <DELL@DESKTOP-7VMN3MI>
2025-03-25 16:53:49 -0400

    se hizo mas legible

--- Models/Citas.cs
+++ Models/Citas.cs
@@ -8,11 +8,12 @@ public class Citas
     [Key]
     public int CitaId { get; set; }
 
+    [Required(ErrorMessage = "La fecha es obligatoria.")]
+    [DataType(DataType.Date)]
     public DateTime Fecha { get; set; }
 
-
     [Required(ErrorMessage = "´Debe introducir el nombre de su mascota.")]
-    public string NombreMascota { get; set; }  
+    public string? NombreMascota { get; set; }  
 
     public int MascotaId { get; set; }
     [ForeignKey("MascotaId")]
