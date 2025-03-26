107444fa98fcbc7bdb79f725ffe3be8e2042fc69
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-03-26 14:06:53 -0400

    Se modifico un poco la clase citas

--- Models/Citas.cs
+++ Models/Citas.cs
@@ -8,9 +8,17 @@ public class Citas
     [Key]
     public int CitaId { get; set; }
 
-    [Required(ErrorMessage = "La fecha es obligatoria.")]
+    [Required(ErrorMessage = "Favor colocar su nombre")]
+    [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El nombre  solo pueden contener letras.")]
+    public string? Nombre { get; set; }
+
+    [Required(ErrorMessage = "La fecha para su cita  es obligatoria.")]
     [DataType(DataType.Date)]
     public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "´Debe escribir una descricion sobre su cita")]
+    public string? Descripcion { get; set; }
+
 
     [Required(ErrorMessage = "´Debe introducir el nombre de su mascota.")]
     public string? NombreMascota { get; set; }  
