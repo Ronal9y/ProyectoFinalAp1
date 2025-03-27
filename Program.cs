847bb0ea47628467fad4cb21e2f2bdb5042fd5f6
DELL <DELL@DESKTOP-7VMN3MI>
2025-03-27 16:49:53 -0400

    se preparo para la implementacion de roles

--- Program.cs
+++ Program.cs
@@ -54,6 +54,7 @@ namespace ProyectoFinalAp1
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddSignInManager()
                 .AddDefaultTokenProviders();
