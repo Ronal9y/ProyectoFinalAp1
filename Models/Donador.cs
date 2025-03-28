4f48595574aaff0afe16e9663eb0f0ea6ceb47ec
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-03-27 21:17:42 -0400

    Se cambio el modelo de usuario a Donador

---
+++ Models/Donador.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp1.Models;
+
+public class Donador
+{
+    [Key]
+    public int DonadorId { get; set; }
+  
+    public string? Nombre { get; set; }
+
+    public string? Direccion { get; set; }
+ 
+    public string? Telefono { get; set; }
+
+    public string? Email { get; set; }
+
+  
+}
