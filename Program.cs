68cc9fc8191c805af025f197911df9c728bf786f
DELL <DELL@DESKTOP-7VMN3MI>
2025-04-01 23:47:44 -0400

    se quito el email y se puso false en el identitycore

--- Program.cs
+++ Program.cs
@@ -50,9 +50,6 @@ namespace ProyectoFinalAp1
             })
                 .AddIdentityCookies();
 
-            //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
